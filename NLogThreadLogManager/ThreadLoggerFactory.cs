747ba9afda9bd77ba745ba18e7c9780c682c24b2
Joey Novak <joey.novak@obd.zone>
2018-01-22 17:05:15 -0700

    Initial Commit

---
+++ NLogThreadLogManager/ThreadLoggerFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NLog;
+
+namespace NLogThreadLogManager
+{
+   public class ThreadLoggerFactory
+   {
+      private readonly ThreadLocal<string> _threadAlias = new ThreadLocal<string>();
+      private readonly Dictionary<string, ILogger> _loggers = new Dictionary<string, ILogger>();
+      private readonly string _fullClassName;
+
+      public ILogger Logger
+      {
+         get
+         {
+            string threadName = ThreadLogManager.ThreadName;
+
+            lock (_loggers)
+            {
+               if (!_loggers.ContainsKey(threadName))
+                  _loggers[threadName] = LogManager.GetLogger(threadName + "." + _fullClassName);
+
+               return _loggers[threadName];
+            }
+
+
+         }
+      }
+
+      public ThreadLoggerFactory(string fullClassName)
+      {
+         this._fullClassName = fullClassName;
+      }
+   }
+}
---
+++ NLogThreadLogManagerTests/ThreadLoggerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NLogThreadLogManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogThreadLogManager.Tests
+{
+   [TestClass()]
+   public class ThreadLoggerTests
+   {
+      [TestMethod()]
+      public void ThreadLoggerTest()
+      {
+         Assert.Fail();
+      }
+   }
+}
